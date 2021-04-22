3c80e65516df1a6aa421ece4c8363216a0f2cadf
datduonguva <dqd8tp@virginia.edu>
2021-04-22 00:57:57 -0400

    more docstring

--- hedgetool/examples/cnn/cnn.py
+++ hedgetool/examples/cnn/cnn.py
@@ -27,14 +27,25 @@ def encode_sentence(sentence, max_length, padding=-1):
         ]
     )
 
-if __name__ == '__main__':
+def main():
+    """
+    To generate the Hierachical Explaination of a model, please extend the HEDGE class and
+    overwrite the `predict_prob` method to return the softmax output given the encoded
+    input
 
+    This step is different for each model.
+    """
+    # load the pretrained model
     model = tf.keras.models.load_model(MODEL_PATH)
-
 
     padding = vocab['#'] 
     max_length = 70
 
+    sentence = 'i think the song sound very interesting'
+    encoded_sentence = encode_sentence(sentence, max_length, padding)
+
+
+    # extend the HEDGE class and overwrite the `predict_prob` method
     class MyHEDGE(HEDGE):
         def __init__(self, *arg):
             super(MyHEDGE, self).__init__(*arg)
@@ -43,11 +54,15 @@ if __name__ == '__main__':
             return self.model.predict(encoded_sentence)
 
     ids_to_tokens = {i:k for k, i in vocab.items()}
+
+    #initialize your extended HEDGE object
     hedge = MyHEDGE(model, max_length, padding, ids_to_tokens)
 
-    sentence = 'i think the song sound very interesting'
-
-    encoded_sentence = encode_sentence(sentence, max_length, padding)
+    # calling the main algorithm 
     P_history, spans, contributions = hedge.main_algorithm(encoded_sentence)
     
+    # visualize the outcome
     hedge.visualize(encoded_sentence, P_history, contributions)
+
+if __name__ == '__main__':
+    main()
